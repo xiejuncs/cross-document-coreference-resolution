5ed25c8c69d5c4621cedb51a6833b18b2253ce0e
xiejuncs <xiejuncs@gmail.com>
2012-08-03 09:58:35 -0700

    add linear regression and update the eecbCorefence file

---
+++ classifier/LinearRegression.java
@@ -0,0 +1,231 @@
+package edu.oregonstate.classifier;
+
+import Jama.Matrix;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * A class to train and evaluate Linear Regression models with L2-Regularization
+ * 
+ * @author Jun Xie (xie@eecs.oregonstate.edu)
+ *
+ */
+public class LinearRegression {
+	public Double lambda;
+	public String trainingFile;
+	public String testingFile;
+	
+	public LinearRegression() {
+		lambda = 1.0;
+		trainingFile = "regressiondata/regression-train.csv";
+		testingFile = "regressiondata/regression-test.csv";
+	}
+	
+	/**
+	 * Read a matrix from a comma sperated file
+	 * 
+	 * @param fileName
+	 * @return
+	 */
+	private Matrix readMatrix(String fileName) {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(fileName));
+			List<double[]> data_array = new ArrayList<double[]>();
+
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String fields[] = line.split(",");
+				double data[] = new double[fields.length];
+				for (int i = 0; i < fields.length; ++i) {
+					data[i] = Double.parseDouble(fields[i]);
+				}
+				data_array.add(data);
+			}
+
+			if (data_array.size() > 0) {
+				int cols = data_array.get(0).length;
+				int rows = data_array.size();
+				Matrix matrix = new Matrix(rows, cols);
+				for (int r = 0; r < rows; ++r) {
+					for (int c = 0; c < cols; ++c) {
+						matrix.set(r, c, data_array.get(r)[c]);
+					}
+				}
+				return matrix;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+	    return new Matrix(0, 0);
+	}
+	
+	/**
+	 * the last column correspond to the target
+	 * Hence, remove target values from the last column of a data set.
+	 * <p>
+	 * Meanwhile, we need to add 1 to 0 column of each row as a bias term 
+	 * 
+	 * @param data_set
+	 * @return
+	 */
+	private Matrix getDataPoints(Matrix data_set) {
+		Matrix features = data_set.getMatrix(0, data_set.getRowDimension() - 1, 0, data_set.getColumnDimension() - 2);
+		int rows = features.getRowDimension();
+		int cols = features.getColumnDimension() + 1;
+		Matrix modifiedFeatures = new Matrix(rows, cols);
+		for (int r = 0; r < rows; ++r) {
+			for (int c = 0; c < cols; ++c) {
+				if (c == 0) {
+					modifiedFeatures.set(r, c, 1.0);
+				} else {
+					modifiedFeatures.set(r, c, features.get(r, c-1));
+				}
+			}
+		}
+		return modifiedFeatures;
+	}
+	
+	/**
+	 * Returns the target values from the last column of a data set.
+	 * 
+	 * @param data_set
+	 * @return
+	 */
+	private Matrix getTargets(Matrix data_set) {
+	    return data_set.getMatrix(0, data_set.getRowDimension() - 1, data_set.getColumnDimension() - 1, data_set.getColumnDimension() - 1);
+	}
+	
+	/**
+	 * train the model using linear regression with L2 regularizer
+	 * <p>
+	 * The first column of the features should be set as all 1. because the first feature should 
+	 * perform as bias
+	 * <p>
+	 * Linear regression with L2 regularizer has the close form as follows:
+	 * w = (x^{T} * X + \lambda * I)^{-1} * X^{T} * t 
+	 * 
+	 * @param data n * m
+	 * @param targets n * 1
+	 * @param lambda
+	 * @return weight m * 1
+	 */
+	private Matrix trainLinearRegressionModel(Matrix data, Matrix targets, Double lambda) {
+		int row = data.getRowDimension();
+		int column = data.getColumnDimension();
+		Matrix identity = Matrix.identity(column, column);
+		identity.times(lambda);
+		Matrix dataCopy = data.copy();
+		Matrix transponseData = dataCopy.transpose();
+		Matrix norm = transponseData.times(data);
+		Matrix circular = norm.plus(identity);
+		Matrix circularInverse = circular.inverse();
+		Matrix former = circularInverse.times(data.transpose());
+		Matrix weight = former.times(targets);
+		
+	    return weight;
+	}
+	
+	/**
+	 * test the model using the weights trained using linear regression with L2 regularizer
+	 * 
+	 * @param data n * m matrix
+	 * @param targets n * 1 matrix
+	 * @param weights m * 1 matrix
+	 * @return
+	 */
+	private double evaluateLinearRegressionModel(Matrix data, Matrix targets, Matrix weights) {
+		double error = 0.0;
+		int row = data.getRowDimension();
+		int column = data.getColumnDimension();
+		assert row == targets.getRowDimension();
+		assert column == weights.getColumnDimension();
+		
+		Matrix predictTargets = predict(data, weights);
+		for (int i = 0; i < row; i++) {
+			error = (targets.get(i, 0) - predictTargets.get(i, 0)) * (targets.get(i, 0) - predictTargets.get(i, 0));
+		}
+
+		return 0.5 * error;
+	}
+	
+	/**
+	 * calcualte the predict targests given features and the learned weights
+	 * 
+	 * @param data a matrix with n * m 
+	 * @param weights a matrix with 1 * m
+	 * @return predict targets according to the weight
+	 */
+	private Matrix predict(Matrix data, Matrix weights) {
+		int row = data.getRowDimension();
+		Matrix predictTargets = new Matrix(row, 1);
+		for (int i = 0; i < row; i++) {
+			double value = multiply(data.getMatrix(i, i, 0, data.getColumnDimension() -1 ), weights);
+			predictTargets.set(i, 0, value);
+		}
+		return predictTargets;
+	}
+	
+	/**
+	 * multiply two matrix with just 1 row and seveal columns
+	 * 
+	 * @param data a matrix with 1 * column 
+	 * @param weights a matrix with 1 * column
+	 * @return
+	 */
+	private Double multiply(Matrix data, Matrix weights) {
+		Double sum = 0.0;
+		int column = data.getColumnDimension();
+		for (int i = 0; i <column; i++) {
+			sum += data.get(0, i) * weights.get(i, 0);
+		}
+		return sum;
+	}
+	
+	/**
+	 * args consists of the training file path and testing file path
+	 * <p>
+	 * If you want to set the lamdab (penalization coefficient), you can go to the Linear Regression constructor
+	 * to update the value of lambda
+	 * <p>
+	 * In addition, you can update the training path and testing path in the same way.
+	 * <p>
+	 * <b>NOTE</b>Because we use the JAMA package, if you use the Eclipse to run the problem, then
+	 * you need to add the jar file into the library classPath.
+	 * Or if you just use javac and java to compile and run the program, remember that you also need to 
+	 * add the classPath into the command line 
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LinearRegression lr = new LinearRegression();
+		try {
+			Matrix training = lr.readMatrix(lr.trainingFile);
+			Matrix testing = lr.readMatrix(lr.testingFile);
+			
+			/** get the actual features, meanwhile add a N*1 column vector with value being all 1 as the first column of the features */
+			Matrix trainingData = lr.getDataPoints(training);
+			Matrix testingData = lr.getDataPoints(testing);
+			
+			Matrix trainingTargets = lr.getTargets(training);
+		    Matrix testingTargets = lr.getTargets(testing);
+		    
+		    // Train the model.
+		    Matrix weights = lr.trainLinearRegressionModel(trainingData, trainingTargets, lr.lambda);
+
+		    // Evaluate the model using training and testing data.
+		    double training_error = lr.evaluateLinearRegressionModel(trainingData, trainingTargets, weights);
+		    double testing_error = lr.evaluateLinearRegressionModel(testingData, testingTargets, weights);
+
+		    System.out.println(training_error);
+		    System.out.println(testing_error);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);			
+		}
+	}
+	
+}
--- ie/dcoref/EecbCoreference.java
+++ ie/dcoref/EecbCoreference.java
@@ -1,11 +1,171 @@
 package edu.oregonstate.ie.dcoref;
 
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+
+import edu.oregonstate.CRC_MAIN;
+import edu.stanford.nlp.dcoref.CorefCluster;
+import edu.stanford.nlp.dcoref.CorefScorer;
+import edu.stanford.nlp.dcoref.Dictionaries;
+import edu.stanford.nlp.dcoref.Document;
+import edu.stanford.nlp.optimization.ScaledSGDMinimizer.weight;
+import edu.stanford.nlp.stats.Counter;
+
+
 /**
- * 
+ * Iterative Entity/Event Resolution(Excerpt from Stanford's paper)
+ * In this stage, given a linear regression model, find out those action merging two clusters
+ * with the highest score (at least 0.5), and then merge the two clusters. 
+ * Update the feature of the corresponding mentions and clusters and then do greedy search 
+ * again. The process terminates until the merge process can not go forward any more.
+ * <p>
+ * In addition, we also need to make sure that this class can be used in the training part.
+ * In the training part, we need to do the iterative merge T epochs to collect the training data.
+ * After data collection, we train a linear regression model.
  * 
  * @author Jun Xie (xie@eecs.oregonstate.edu)
  *
  */
 public class EecbCoreference {
+	
+	/** Clusters for coreferent mentions */
+	public Map<Integer, CorefCluster> corefClusters;
 
+	/** Gold Clusters for coreferent mentions */
+	public Map<Integer, CorefCluster> goldCorefClusters;
+	
+	/** linear regression model represented as double list*/
+	private Map<String, Double> weights;
+	
+	
+	/** initialize the corefClusters and goldCorefClusters */
+	public EecbCoreference(Document document, Map<String, Double> weights) {
+		this.corefClusters = document.corefClusters;
+		this.goldCorefClusters = document.goldCorefClusters;
+		this.weights = weights;
+	}
+	
+	/**
+	 * Represent two coref clusters we need to compair
+	 * 
+	 * @author Jun Xie (xie@eecs.oregonstate.edu)
+	 *
+	 */
+	public static class ClusterPair {
+		public CorefCluster e1;
+		public CorefCluster e2;
+		
+		public ClusterPair(CorefCluster e1, CorefCluster e2) {
+			this.e1 = e1;
+			this.e2 = e2;
+		}
+	}
+	
+	/** 
+	 * use the CRC_main logger in order to output the result into the log file,
+	 * if uses different logger, then the log file maybe not contain the log produced in this file
+	 * <p>
+	 * merge 2 clusters: to = to + from
+	 * 
+	 */
+	private static Logger logger = CRC_MAIN.logger;
+	
+	public void merge(Document document, boolean gold, Dictionaries dictionaries) {
+		List<CorefCluster> partialCorefClusters = new ArrayList<CorefCluster>();
+		for (Integer key : corefClusters.keySet()) {
+			partialCorefClusters.add(corefClusters.get(key));
+		}
+		
+		HashMap<ClusterPair, Double> pairScore = new HashMap<EecbCoreference.ClusterPair, Double>();
+		int clusterSize = partialCorefClusters.size();
+		for (int i = 0; i < (clusterSize - 2); i++) {
+			for (int j = (i + 1); j < (clusterSize - 1); j++) {
+				CorefCluster e1 = partialCorefClusters.get(i);
+				CorefCluster e2 = partialCorefClusters.get(j);
+				ClusterPair pair = new ClusterPair(e1, e2);
+				
+				// get the feature when compared the too clusters and use features counter to multiply the weight in order to get a score
+				Counter<String> features = Feature.getFeatures(document, e1, e2, gold, dictionaries);
+				double score = calculateScore(features, weights);
+				if (score > 0.5) {
+					pairScore.put(pair, score);
+				}
+			}
+		}
+		while (pairScore.size() > 0 ) {
+			ClusterPair index = compare_hashMap(pairScore);
+			CorefCluster e1 = index.e1;
+			CorefCluster e2 = index.e2;
+			int removeID = e1.getClusterID();
+			CorefCluster.mergeClusters(e1, e2);
+			corefClusters.remove(removeID);
+			
+			pairScore = new HashMap<EecbCoreference.ClusterPair, Double>();
+			clusterSize = partialCorefClusters.size();
+			for (int i = 0; i < (clusterSize - 2); i++) {
+				for (int j = (i + 1); j < (clusterSize - 1); j++) {
+					e1 = partialCorefClusters.get(i);
+					e2 = partialCorefClusters.get(j);
+					ClusterPair pair = new ClusterPair(e1, e2);
+					
+					// get the feature when compared the too clusters and use features counter to multiply the weight in order to get a score
+					Counter<String> features = Feature.getFeatures(document, e1, e2, gold, dictionaries);
+					double score = calculateScore(features, weights);
+					if (score > 0.5) {
+						pairScore.put(pair, score);
+					}
+				}
+			}
+		}	
+	}
+	
+	/**
+	 * iterate the hashmap and find which key has the largest value
+	 * 
+	 * @param scores
+	 * @return
+	 */
+	public ClusterPair compare_hashMap(HashMap<ClusterPair, Double> scores) {
+		Collection c = scores.values();
+		Double maxvalue = Collections.max(c);
+		ClusterPair maxIndex = new ClusterPair(null, null);
+		Set<ClusterPair> scores_set = scores.keySet();
+		Iterator<ClusterPair> scores_it = scores_set.iterator();
+		while(scores_it.hasNext()) {
+			ClusterPair id = scores_it.next();
+			Double value = scores.get(id);
+			if (value == maxvalue) {
+				maxIndex = id;
+				break;
+			}
+		}
+		return maxIndex;
+	}
+	
+	/** 
+	 * calculate the score for a cluster pair
+	 *
+	 * @param features a list of features, represented as its defined counter data structure
+	 * @param weights a list of features, its also represented by a hashmap, the reason for not using array is that we do not the index of features
+	 * 
+	 * @return score   multiply the features and weights
+	 */
+	private Double calculateScore(Counter<String> features, Map<String, Double> weights) {
+		Double score = 0.0;
+		assert features.size() == weights.size();
+		for (String key : features.keySet()) {
+			double counter = features.getCount(key);
+			double weight = weights.get(key);
+			score += counter * weight;
+		}
+		return score;
+	}
+	
 }
